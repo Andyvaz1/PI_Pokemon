29c2669aab578eeb33093bf0a62fd4fbdbdefaa1
Andyvaz1 <andyvaz192@gmail.com>
2022-10-26 13:42:40 -0300

    borre actions obsoletas

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -7,14 +7,7 @@ export const GET_POKEMON_DETAIL = "GET_POKEMON_DETAIL";
 export const GET_ALL_TYPES = "GET_ALL_TYPES";
 export const CREATE_POKEMON = "CREATE_POKEMON";
 export const SEARCH_POKEMON = "SEARCH_POKEMON";
-export const FILTER_TYPE = "FILTER_TYPE";
-export const FILTER_ORIGIN = "FILTER_ORIGIN";
-export const FILTER_ALPHA_AZ = "FILTER_ALPHA_AZ";
-export const FILTER_ALPHA_ZA = "FILTER_ALPHA_ZA";
-export const FILTER_ATTACK_ASC = "FILTER_ATTACK_ASC";
-export const FILTER_ATTACK_DESC = "FILTER_ATTACK_DESC";
 export const NOT_FOUND = "NOT_FOUND";
-
 //////////////ACTION CREATORS//////////////////
 
 //////////////TRAER TODOS LOS POKEMONS///////////////
@@ -84,47 +77,3 @@ export const searchPokemon = (name) => {
             .catch((error) => dispatch({ type: NOT_FOUND, payload: error }));
     };
 };
-
-////////FILTRO TYPE//////////
-
-export const filterType = (type) => {
-    return {
-        type: FILTER_TYPE,
-        t: type,
-    };
-};
-
-//////FILTRO CREATED/////////
-
-export const filterOrigin = (dB) => {
-    return {
-        type: FILTER_ORIGIN,
-        payload: dB,
-    };
-};
-
-////Ordenar por: //////////////
-
-export function filterAlphaAZ() {
-    return {
-        type: FILTER_ALPHA_AZ,
-    };
-}
-
-export function filterAlphaZA() {
-    return {
-        type: FILTER_ALPHA_ZA,
-    };
-}
-
-export function filterAttackAsc() {
-    return {
-        type: FILTER_ATTACK_ASC,
-    };
-}
-
-export function filterAttackDesc() {
-    return {
-        type: FILTER_ATTACK_DESC,
-    };
-}
--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -3,12 +3,6 @@ import {
     GET_ALL_TYPES,
     GET_POKEMON_DETAIL,
     CREATE_POKEMON,
-    FILTER_TYPE,
-    FILTER_ORIGIN,
-    FILTER_ALPHA_AZ,
-    FILTER_ALPHA_ZA,
-    FILTER_ATTACK_ASC,
-    FILTER_ATTACK_DESC,
     NOT_FOUND,
 } from "./actions";
 
@@ -40,97 +34,6 @@ export const rootReducer = (state = initialState, action) => {
                 pokemonDetail: action.payload,
             };
         }
-        //////////////Filter/////////////////
-        case FILTER_TYPE: {
-            let pokemonByType = action.payload.filter((pokemon) => {
-                return pokemon.types.find((t) => {
-                    return t === action.t;
-                });
-            });
-            if (pokemonByType.length > 0) {
-                return {
-                    ...state,
-                    allPokemons: pokemonByType,
-                };
-            } else {
-                return {
-                    ...state,
-                    allPokemons: ["Pokemon Not Found"],
-                };
-            }
-        }
-
-        case FILTER_ORIGIN: {
-            return {
-                ...state,
-                allPokemons: state.allPokemons.filter((pokemon) => {
-                    return pokemon.dataBase === action.payload;
-                }),
-            };
-        }
-
-        case FILTER_ALPHA_AZ: {
-            return {
-                ...state,
-                allPokemons: state.allPokemons.sort((a, b) => {
-                    if (a.name > b.name) {
-                        return 1;
-                    }
-                    if (a.name < b.name) {
-                        return -1;
-                    }
-
-                    return 0;
-                }),
-            };
-        }
-
-        case FILTER_ALPHA_ZA:
-            return {
-                ...state,
-                allPokemons: state.allPokemons
-                    .sort((a, b) => {
-                        if (a.name > b.name) {
-                            return 1;
-                        }
-                        if (a.name < b.name) {
-                            return -1;
-                        }
-
-                        return 0;
-                    })
-                    .reverse(),
-            };
-
-        case FILTER_ATTACK_DESC:
-            return {
-                ...state,
-                allPokemons: state.allPokemons.sort((a, b) => {
-                    if (parseFloat(a.attack) > parseFloat(b.attack)) {
-                        return 1;
-                    }
-                    if (parseFloat(a.attack) < parseFloat(b.attack)) {
-                        return -1;
-                    }
-
-                    return 0;
-                }),
-            };
-
-        case FILTER_ATTACK_ASC:
-            return {
-                ...state,
-                allPokemons: state.allPokemons.sort((a, b) => {
-                    if (parseFloat(a.attack) < parseFloat(b.attack)) {
-                        return 1;
-                    }
-                    if (parseFloat(a.attack) > parseFloat(b.attack)) {
-                        return -1;
-                    }
-
-                    return 0;
-                }),
-            };
 
         case NOT_FOUND:
             return {
